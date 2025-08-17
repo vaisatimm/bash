193774f66a4575ea87c8a7fe16fa897ced687aee
Claudio Palmisano <claudio.palmisano@capgemini.com>
2025-08-17 21:20:56 +0200

    Update

---
+++ 13-semi-colon.sh
@@ -0,0 +1,5 @@
+If you use the semicolon between two commands, the second will be always executed even if the previous fail.
+
+ls /etc/mannaggiasangennaro; ls /etc/hosts
+
+The 1st will fail and the second will get always executed. 
