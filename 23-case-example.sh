e0a883e97290ce45c591f73d1aae23f4d4a0044a
Claudio Palmisano <claudio.palmisano@capgemini.com>
2025-08-20 19:13:40 +0200

    Update

---
+++ 23-case-example.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+case "$1" in
+    hello)
+      echo "Hello, welcome!"
+      ;;
+    bye)
+      echo "Bye!"
+      ;;
+    gennaro)
+      echo "A puttan e mammt!!!"
+      ;;
+esac	
